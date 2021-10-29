4421f990ffbd80c6005ff21ceaef73b33a6c00f2
LataouiFz <ing19lataoui@gmail.com>
2021-10-29 11:55:50 +0200

    commit du vendredu

--- src/app/tousdebats/tousdebats.component.ts
+++ src/app/tousdebats/tousdebats.component.ts
@@ -6,6 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tousdebats.component.css']
 })
 export class TousdebatsComponent implements OnInit {
+ 
+  name = 'Angular';
+  imageObject = [{
+      Image: "../../assets/images/live.png",
+      thumbImage: '../../assets/images/live.png',
+      title: 'Sujet1'
+  }, {
+      image: '../../assets/images/live.png',
+      thumbImage: '../../assets/images/live.png',
+     title: 'Sujet'
+  }, {
+      image: '../../assets/images/live.png',
+      thumbImage: '../../assets/images/live.png',
+      title: 'Sujet3'
+  },{
+      image: '../../assets/images/live.png',
+      thumbImage: '../../assets/images/live.png',
+      title: 'Sujet4'
+  },{
+      image: '../../assets/images/live.png',
+      thumbImage: '../../assets/images/live.png',
+      title:'Sujet5'
+  },{
+      image: '../../assets/images/live.png',
+      thumbImage: '../../assets/images/live.png',
+      title:'Sujet6'
+  },{
+      image: '../../assets/images/live.png',
+      thumbImage: '../../assets/images/live.png',
+      title:'Sujet7'
+  }
+
+];
 
    constructor() { }
 
